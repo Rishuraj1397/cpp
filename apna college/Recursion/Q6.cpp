974ecf50330d9b802e90cdef469d17599f5ac0e2
Rishu Raj <143050346+Rishuraj1397@users.noreply.github.com>
2026-02-14 21:37:12 +0530

    ..

---
+++ apna college/Recursion/Q6.cpp
@@ -0,0 +1,23 @@
+// sum of 1 to n numbers using recursion
+
+#include <iostream>
+using namespace std;
+
+void Add(int i,int sum){
+    if (i < 1)
+    {
+        cout<< sum <<endl;
+        return;
+    }
+    Add(i-1, sum+i);
+    
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of N: ";
+    cin>>n;
+    Add(n,0);
+    return 0;
+}
