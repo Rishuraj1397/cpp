c305242d6f65af28d6dd60499873ed1a4edb92f2
Rishu Raj <rishuraj1397@gmail.com>
2026-03-26 01:34:08 +0530

    basic of ll almost done

--- rohit negi/Linked List/LL2.cpp
+++ rohit negi/Linked List/LL2.cpp
@@ -15,10 +15,47 @@ public:
 };
 int main()
 {
-    Node *Head;
-    Head = new Node(4);
+    Node *Head = NULL;
+    // Head = new Node(4);
 
-    cout<<Head->data<<endl;
-    cout<<Head->next;
+    // cout<<Head->data<<endl;
+    // cout<<Head->next;
+
+    int arr[] = {2, 4, 6, 8, 10};
+
+    //  Inserting the node at starting
+
+    // in case of first element when there is no element in the linklist
+
+    for (int i = 0; i < 5; i++)
+    {
+        {
+            /* code */
+        }
+
+        if (Head == NULL)
+        {
+            Head = new Node(arr[i]);
+        }
+
+        // In case of if there is any elemnt in the linkedlist
+
+        else
+        {
+            Node *temp;
+            temp = new Node(arr[i]);
+            temp->next = Head;
+            Head = temp;
+        }
+    }
+
+
+
+    Node *temp = Head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+
     return 0;
-} 
+}
