570b3b56630f9e3aa525439b9ef649ef76e9fc31
Rishu <rishuraj1397@gmail.com>
2026-03-27 19:54:03 +0530

    ..

---
+++ apna college/oops.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    double cgpa;
+
+
+
+     Student 
+
+    int age;
+    string course;
+
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Course: " << course << endl;
+    }
+};
+
+int main() {
+
+
+    return 0;
+}
