c6e61825057941e3abee323f35526736cc791e9d
Rishu Raj <rishuraj1397@gmail.com>
2026-04-02 11:21:28 +0530

    adding oops

--- apna college/Inheritance.cpp
+++ apna college/Inheritance.cpp
@@ -9,16 +9,45 @@ public:
     
     
     Person (string name, int age){
+        // cout<< " Parents Constructor called : "<< endl;
         this->name = name;
         this->age = age;
     }
+
+    // ~Person(){
+    //     cout << "Parents Destructor called : "<< endl;
+    // }
+
+    // Person(){
+
+    // }
 };
 
 class Student :public Person{
     //name,age,rollno
+public:
+    int roll_no;
+    
+    Student(string name, int age, int roll_no) : Person(name, age){
+        // cout << " Child Constructor called!" << endl;
+        this->roll_no = roll_no;
+    }
+
+    // ~Student(){
+    //     cout << "child Destructor called"<< endl;
+    // }
+    
+    void getinfo(){
+        cout << "name: " << name << endl;
+        cout << "age: " << age << endl;
+        cout << "rollNo: " << roll_no << endl;
+    }
 };
 int main()
 {
-    
+    Student s1("rishu", 21, 41);
+
+    s1.getinfo();
+
     return 0;
 }
