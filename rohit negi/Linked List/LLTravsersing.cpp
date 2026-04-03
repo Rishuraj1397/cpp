b6a22ed0ced5790022ebe2c001ff3fbbd8fc170f
Rishu Raj <rishuraj1397@gmail.com>
2026-04-03 19:02:59 +0530

    traversal

---
+++ rohit negi/Linked List/LLTravsersing.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    public:
+    Node(int data, Node* next){
+        this->data = data;
+        this->next = next;
+    }
+
+    Node(int data){
+        this->data = data;
+        this->next = nullptr;
+    }
+
+};
+
+Node* convertArr2LL(vector<int> arr) {
+    if (arr.empty()) {
+        return nullptr;
+    }
+    Node* head = new Node(arr[0]);
+    Node* current = head;
+    for (int i = 1; i < arr.size(); i++) {
+        Node* temp = new Node(arr[i]);
+        current->next = temp;
+        current = temp;
+    }
+    return head;
+};
+
+
+int main(){
+    vector<int> arr = {12, 5, 6, 8, 7};
+    Node* head = convertArr2LL(arr);
+    Node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    return 0;
+}
