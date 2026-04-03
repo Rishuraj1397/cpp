5fc9478d769a61509faeee9886743d1de9329f9d
Rishu Raj <rishuraj1397@gmail.com>
2026-04-03 18:58:43 +0530

    updated

--- rohit negi/Linked List/ArrayToLinklist.cpp
+++ rohit negi/Linked List/ArrayToLinklist.cpp
@@ -22,25 +22,21 @@ public:
 };
 
 Node* convertArr2LL(vector<int> arr) {
-    // if (arr.empty()) {
-    //     return nullptr;
-    // }
+    if (arr.empty()) {
+        return nullptr;
+    }
     Node* head = new Node(arr[0]);
     Node* current = head;
     for (int i = 1; i < arr.size(); i++) {
-        current->next = new Node(arr[i]);
-        current = current->next;
+        Node* temp = new Node(arr[i]);
+        current->next = temp;
+        current = temp;
     }
     return head;
 };
 
 int main() {
-    vector<int> arr = {2, 5, 6, 8, 7};
+    vector<int> arr = {12, 5, 6, 8, 7};
     Node* head = convertArr2LL(arr);
-    Node* current = head;
-    while (current != nullptr) {
-        cout << current->data << " ";
-        current = current->next;
-    }
-    return 0;
+    cout <<head->data;
 }
